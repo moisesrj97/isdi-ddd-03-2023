ba1556a62a83e75acf115ef53ce342fc3d2d8582
Moisés Rodríguez <65286072+moisesrj97@users.noreply.github.com>
2023-02-08 12:56:45 +0100

    feat: add base models

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,29 @@
+class User {
+  constructor(
+    public id: string,
+    public name: string,
+    public surname: string,
+    public email: string,
+    public phone: string,
+    public bootCampYear: number,
+    public campus: 'MAD' | 'BCN' | 'MAL'
+  ) {}
+}
+
+class TechnicalTest {
+  constructor(
+    public id: string,
+    public studentId: string,
+    public company: string,
+    public uploadDate: Date,
+    public role:
+      | 'Frontend'
+      | 'Backend'
+      | 'Fullstack'
+      | 'Mobile'
+      | 'UX/UI'
+      | 'DevOps'
+  ) {}
+}
+
 console.log('Hello World');
